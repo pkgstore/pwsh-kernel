5a3b38398a421365eaf7bcf699024542a327c070
KitsuneSolar <kitsune.solar@gmail.com>
2023-11-18 18:16:06 +0000

    2023-11-18 18:16:06

--- PkgStore.Kernel.psm1
+++ PkgStore.Kernel.psm1
@@ -1,13 +1,16 @@
+$ModuleManifest = (Get-ChildItem -Path $PSScriptRoot | Where-Object {$_.Extension -eq '.psd1'})
+$CurrentManifest = (Test-ModuleManifest $ModuleManifest)
+
 $Aliases = @()
-$PrivateFunctions = (Get-ChildItem -Path (Join-Path $PSScriptRoot 'Private') -Include '*.ps1' -File -Recurse)
-$PublicFunctions = (Get-ChildItem -Path (Join-Path $PSScriptRoot 'Public') -Include '*.ps1' -File -Recurse)
+$PrivateFunctions = (Get-ChildItem -Path (Join-Path $PSScriptRoot 'Private') | Where-Object {$_.Extension -eq '.ps1'})
+$PublicFunctions = (Get-ChildItem -Path (Join-Path $PSScriptRoot 'Public') | Where-Object {$_.Extension -eq '.ps1'})
 
 (@($PrivateFunctions) + @($PublicFunctions)) | ForEach-Object {
   try {
-    Write-Verbose "Loading '$($_.FullName)'."
+    Write-Verbose "Loading: '$($_.FullName)'."
     . $_.FullName
   } catch {
-    Write-Warning $_.Exception.Message
+    $_.Exception.Message | Write-Warning
   }
 }
 
@@ -19,5 +22,30 @@ $PublicFunctions = (Get-ChildItem -Path (Join-Path $PSScriptRoot 'Public') -Incl
     Export-ModuleMember -Function $_.BaseName -Alias $Alias
   } else {
     Export-ModuleMember -Function $_.BaseName
+  }
+}
+
+$FunctionsAdded = ($PublicFunctions | Where-Object {$_.BaseName -notin $CurrentManifest.ExportedFunctions.Keys})
+$FunctionsRemoved = ($CurrentManifest.ExportedFunctions.Keys | Where-Object {$_ -notin $PublicFunctions.BaseName})
+$AliasesAdded = ($Aliases | Where-Object {$_ -notin $CurrentManifest.ExportedAliases.Keys})
+$AliasesRemoved = ($CurrentManifest.ExportedAliases.Keys | Where-Object {$_ -notin $Aliases})
+
+if ($FunctionsAdded -or $FunctionsRemoved -or $AliasesAdded -or $AliasesRemoved) {
+  try {
+    $UpdateModuleManifestParams = @{}
+    $UpdateModuleManifestParams.Add('Path', $ModuleManifest)
+    $UpdateModuleManifestParams.Add('ErrorAction', 'Stop')
+
+    if ($Aliases.Count -gt 0) {
+      $UpdateModuleManifestParams.Add('AliasesToExport', $Aliases)
+    }
+
+    if ($PublicFunctions.Count -gt 0) {
+      $UpdateModuleManifestParams.Add('FunctionsToExport', $PublicFunctions.BaseName)
+    }
+
+    Update-ModuleManifest @UpdateModuleManifestParams
+  } catch {
+    $_ | Write-Error
   }
 }
